c4b1ccd161f217bc2529aa3ce731eb4d8a9bbc30
Heider Rivas <heiderarb@gmail.com>
2024-10-16 17:08:54 -0500

    postman tests

--- ap/routes/products.js
+++ ap/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Product = require('/ap/models/product');
+const Product = require('../models/product');
 let products = [];
 
 // Validations
