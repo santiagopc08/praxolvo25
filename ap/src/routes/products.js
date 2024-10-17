589ef8b90c05c4de7a81426a529437ce38b5af79
Jonatan Chavarriaga <jonatanchavarriaga@gmail.com>
2024-10-17 01:57:34 -0500

    Add validation middlewares for product data with multi-error handling

--- ap/src/routes/products.js
+++ ap/src/routes/products.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { productsController } = require('../controllers');
+const {validateProduct} = require('../middleware');
+
 
 router.get('/',productsController.getProducts);
-router.post('/',productsController.CreateProduct);
+router.post('/',validateProduct,productsController.CreateProduct);
 router.get('/:id',productsController.getProductById);
-router.put('/:id',productsController.UpdateProduct);
+router.put('/:id',validateProduct,productsController.UpdateProduct);
 router.delete('/:id',productsController.DeleteProduct);
 
 module.exports = router;
