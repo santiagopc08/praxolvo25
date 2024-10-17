f3b09ca840cde8fdfac72477ad9e67adcba66987
Mariana0908 <marianaospinahenao0908@gmail.com>
2024-10-17 00:22:42 -0500

    feat:Include error handlers for each validation

--- ap/routes/products.js
+++ ap/routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-let products = [
+const products = [
   {
     id: 1,
     name: "ball",
@@ -40,14 +40,21 @@ let products = [
 
 // GET /products - Get all products
 router.get("/", (req, res) => {
+  if (products.length === 0) {
+    return res.status(200).send("There are no products");
+  }
   res.status(200).json(products);
 });
 
 // GET /products - Get products by id
 router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const found = products.find((product) => product.id == id);
-  res.status(200).json(found);
+  const { id } = req.params;
+  const foundProductById = products.find((product) => product.id === parseInt(id));
+  //create a validation for id does't exist
+  if (!foundProductById) {
+    return res.status(404).send("Product not found");
+  }
+  res.status(200).json(foundProductById);
 });
 
 // POST /products - Create product
@@ -60,31 +67,66 @@ router.post("/", (req, res) => {
     price,
     creationDate: new Date(),
   };
+  //create a validation for required fields
+  if (!name || !descr || !price) {
+    return res.status(400).send("Missing required fields");
+  }
+  // create a validation for price different to a number
+  if (typeof price !== "number") {
+    return res.status(400).send("Price must be a number");
+  }
+  //create a validation for price less than 0
+  if (price <= 0) {
+    return res.status(400).send("Price must be greater than 0");
+  }
+
   products.push(newProduct);
   res.status(201).json(newProduct);
 });
-// UPDATE /products - Update products by id
-router.patch("/:id", (req, res) => {
-  const id = req.params.id;
-  const { name, descr, price } = req.body;
-  const productDate = products.find((product) => product.id == id);
 
-  const productIndex = products.findIndex((product) => product.id == id);
+// UPDATE /products - Update products by id
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, descr, price } = req.body;
+  const product = products.find((product) => product.id === parseInt(id));
+  const productIndex = products.findIndex((product) => product.id === parseInt(id));
   products[productIndex] = {
     id,
     name,
     descr,
     price,
-    creationDate: productDate.creationDate
+    creationDate: new Date(),
   };
+
+  //create a validation for id does't exist
+  if (!product) {
+    return res.status(404).send("Product not found");
+  }
+  //create a validation for required fields
+  if (!name &&!descr &&!price) {
+    return res.status(400).send("No fields to update");
+  }
+  // create a validation for price different to a number
+  if (typeof price !== "number") {
+    return res.status(400).send("Price must be a number");
+  }
+  //create a validation for price less than 0
+  if (price <= 0) {
+    return res.status(400).send("Price must be greater than 0");
+  }
+
   res.status(200).json(products);
 });
 
 // DELETE /products - Delete products by id
 router.delete("/:id", (req, res) => {
   const id = req.params.id;
-  const remove = products.filter((product) => product.id != id);
-  res.status(200).json(remove);
+  const newProductList = products.filter((product) => product.id != id);
+  //create a validation for the id don't exist
+  if (products.length === newProductList.length) {
+    return res.status(404).send("Product not found");
+  } 
+  res.status(200).json(newProductList);
 });
 
 module.exports = router;
